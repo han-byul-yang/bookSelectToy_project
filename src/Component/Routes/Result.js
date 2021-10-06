f4a6cd710b099b76e0a6e38e803272c0cb004cd4
Han Byul <yahb9@naver.com>
2021-10-06 22:29:27 +0900

    api problem

--- src/Component/Routes/Result.js
+++ src/Component/Routes/Result.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import api from '../../Component/api'
 import styled from 'styled-components'
 import {state} from './Home/Homecontainer'
+import axios from 'axios'
 
 const Component = styled.div`
 width: 600px;
@@ -16,12 +16,32 @@ const Head = styled.div`
 font-size: 20px;
 margin-top: 20px;`
 
-const Result = ({state}) => {
-    return (
-        <Component>
-            <Head>{state}님의 인생 책은...</Head>
-        </Component>
-    )
+export default class Result extends React.Component{
+        state = {
+            book: []
+        }
+    
+    
+    
+   async componentDidMount(){
+        /*await axios({
+            method: 'GET',
+            url: 'https://cors-anywhere.herokuapp.com/https://nl.go.kr/NL/search/openApi/saseoApi.do',
+            headers:{
+                'APIKey': 'eb3730ce2882243fb834d7bafc5a12b313b640d85279fe815d81821905ef8e7c'
+            }
+        }).then((response)=>console.log(response))
+          */
+         await axios.get('https://cors-anywhere.herokuapp.com/https://nl.go.kr/NL/search/openApi/saseoApi.do?key=eb3730ce2882243fb834d7bafc5a12b313b640d85279fe815d81821905ef8e7c')
+         
+    }
+    render(){
+        return (
+            <Component>
+                <Head>님의 인생 책은...</Head>
+            </Component>
+        )
 }
 
-export default Result
+
+}
---
+++ src/Component/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+const Api = () => {
+    const api = () => {axios.get("https://nl.go.kr/NL/search/openApi/saseoApi.do?key=eb3730ce2882243fb834d7bafc5a12b313b640d85279fe815d81821905ef8e7c")
+    return api}
+}
+
+export default Api
